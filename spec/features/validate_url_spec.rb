769a997af166ce3de1701038177bb4e6134ff2bd
Alfie Darko <me@alfiedarko.co.uk>
2018-03-13 16:54:08 +0000

    passed rspec tests for flash error messages

--- spec/features/validate_url_spec.rb
+++ spec/features/validate_url_spec.rb
@@ -3,6 +3,13 @@ feature 'validate links' do
     visit('/')
     fill_in 'add_link', with: 'htp::/wesbos.com'
     click_button 'submit_link'
-    expect(page).to have_content 'Invalid URL'
+    expect(page).to have_content 'Incorrect URL! Try again'
+  end
+
+  scenario 'user adds valid link' do
+    visit('/')
+    fill_in 'add_link', with: 'http://www.wesbos.com'
+    click_button 'submit_link'
+    expect(page).to have_content 'URL added to Bookie!'
   end
 end
