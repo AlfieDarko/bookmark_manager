ab9cb6b7a6c9287c5e9bf1709db58ac49e866092
Alfie Darko <me@alfiedarko.co.uk>
2018-03-13 14:46:15 +0000

    added rescue clauses to rakefile code

--- rakefile.rb
+++ rakefile.rb
@@ -17,6 +17,37 @@ task :test_environment do
   connection.exec("INSERT INTO links VALUES(1, 'http://www.makersacademy.com');")
   connection.exec("INSERT INTO links VALUES(2, 'http://www.google.com');")
   connection.exec("INSERT INTO links VALUES(3, 'http://www.facebook.com');")
-
   p 'complete'
+end
+
+desc 'Sets up test and development environment'
+task :setup do
+  begin
+    p 'Creating databases...'
+
+    connection = PG.connect
+    connection.exec('CREATE DATABASE bookmark_manager;')
+  rescue StandardError
+    p 'Development Database already exists'
+  end
+
+  begin
+    connection.exec('CREATE DATABASE bookmark_manager_test;')
+  rescue StandardError
+    p 'Test Database already exists'
+  end
+
+  begin
+    connection = PG.connect(dbname: 'bookmark_manager')
+    connection.exec('CREATE TABLE links(id SERIAL PRIMARY KEY, url VARCHAR(60));')
+  rescue StandardError
+    p 'Links table already exists in Development Database'
+  end
+
+  begin
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec('CREATE TABLE links(id SERIAL PRIMARY KEY, url VARCHAR(60));')
+  rescue StandardError
+    p 'Links table already exists in Test Database'
+  end
 end
